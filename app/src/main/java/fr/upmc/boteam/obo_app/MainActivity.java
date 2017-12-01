6ab8e59f2c0299b4a38db78170918967ca596e4f
leor <leo@leonor.com>
2017-12-01 15:06:34 +0100

    refresh from instial project

--- app/src/main/java/fr/upmc/boteam/obo_app/MainActivity.java
+++ app/src/main/java/fr/upmc/boteam/obo_app/MainActivity.java
@@ -1,11 +1,14 @@
 package fr.upmc.boteam.obo_app;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,11 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText mPass;
 
     private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 112;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 113;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 114;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
 
         mLogin = (EditText) findViewById(R.id.et_login);
         mPass = (EditText) findViewById(R.id.et_pass);
@@ -48,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
                 client.sendAssociationRequest();
 
-                checkPermission();
+
 
                 client.testVideo(getApplicationContext());
 
@@ -60,50 +67,62 @@ public class MainActivity extends AppCompatActivity {
 
                 client.testEmit();
 
+                recordVideo(v);
             }
         });
     }
 
-    private void checkPermission() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
+    public void recordVideo(View view) {
+        Intent intent = new Intent(this, VideoCapture.class);
+        startActivity(intent);
+    }
+
+    public void checkPermission() {
+        if(ContextCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
+                != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                Log.e("WRITE_EXTERNAL_STORAGE", "permission denied");
+            }else{
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            }
+        }
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)){
+                Log.e("RECORD_AUDIO", "permission denied");
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+            }
+        }
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)){
+                Log.e("CAMERA", "permission denied");
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
             }
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
         }
     }
 
