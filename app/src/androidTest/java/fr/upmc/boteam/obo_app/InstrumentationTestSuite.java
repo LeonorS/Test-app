3a5543e390e15dfd8d3738ac593381229f9f6d46
leor <leo@leonor.com>
2017-12-11 19:26:56 +0100

    push remote branch

--- app/src/androidTest/java/fr/upmc/boteam/obo_app/InstrumentationTestSuite.java
+++
@@ -1,9 +0,0 @@
-package fr.upmc.boteam.obo_app;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({MainActivityTest.class , VideoCaptureTest.class})
-public class InstrumentationTestSuite {
-}
